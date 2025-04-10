2bb4788b33659c6e089b2363660e1e0b2d7af927
Tissi-303 <leticiasantosjp25@gmail.com>
2025-04-09 21:26:19 -0300

    Add files via upload

---
+++ exercicio17.py
@@ -0,0 +1,7 @@
+#Utilizando a lista do exercício anterior, exiba uma saudação ("Olá como vai você"),
+#personalizado com o nome de cada amigo.
+#A saudação deve ser a mesma, alterando apenas o nome do amigo.
+
+nomes_amigos = ['Sarah', 'Leticia', 'Victoria', 'Davi']
+for nome in nomes_amigos:
+    print(f'Olá {nome}, como você está?')
