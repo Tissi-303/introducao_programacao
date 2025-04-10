a186a9dbe052b7ce83514f4df082839d554acb86
Tissi-303 <leticiasantosjp25@gmail.com>
2025-04-09 21:23:55 -0300

    Add files via upload

---
+++ exercicio16.py
@@ -0,0 +1,9 @@
+#Armazene os nomes de alguns de seus amigos em uma lista chamada amigos.
+#Exiba o nome de cada pessoa acessando cada elemento da lista um de cada vez.
+
+#for
+
+nomes_amigos = ['Sarah', 'Leticia', 'Victoria', 'Davi']
+
+for nome in nomes_amigos:
+    print(nome)
