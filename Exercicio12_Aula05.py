f2cfed0dcbad0b2e52b72d5ac8bc8f1b04f87e70
Tissi-303 <leticiasantosjp25@gmail.com>
2025-04-23 19:34:49 -0300

    Add files via upload

---
+++ Exercicio12_Aula05.py
@@ -0,0 +1,12 @@
+lado1 = int(input('Digite o lado A: '))
+lado2 = int(input('Digite o lado B: '))
+lado3 = int(input('Digite o lado C: ')) 
+
+if lado1 == lado2 == lado3
+    print('Seu triangulo é equilátero!!')
+
+elif lado1 == lado2 or lado3 == lado1 or lado3 == lado2
+    print('Seu triangulo é Isósceles!')
+
+else 
+    print('Seu Triangulo é Escaleno!')
