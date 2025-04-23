079af97e4d28ade5ae99fe71f5c64df539f257ef
Tissi-303 <leticiasantosjp25@gmail.com>
2025-04-23 20:48:55 -0300

    Add files via upload

---
+++ pesquisando_em_um_vetor.py
@@ -0,0 +1,27 @@
+from random import randint
+
+q = []
+
+for n in range(20):
+    numero = randint(a:0, b:1000)
+    q.append(numero)
+
+maior = -1
+inidice_maior = -1
+menor = 1000
+indice_menor = -1
+
+for i in range(len(q))
+    if  q[i] > maior:
+        maior = q[i]
+        inidce_maior = if
+
+    if q[i] < menor:
+        menor = q[i]
+        indice_menor = i
+
+print [q]
+print(f'O maior valor é {maior}')
+print(f'O índice do maior valor é {inidce_maior}')
+print(f'O menor valor é {menor}')
+print(f' O índice do menor valor é {indice_menor}')
