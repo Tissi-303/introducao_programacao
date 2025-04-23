6db66cc35903b8ae0a7b9825ed449522a06c4310
Tissi-303 <leticiasantosjp25@gmail.com>
2025-04-23 19:43:29 -0300

    Add files via upload

---
+++ Exercicio13_aula05.py
@@ -0,0 +1,12 @@
+Salario = float(input('Digite seu salário: '))
+
+if Salario <= 2000
+    print('Salário isento de impsto de renda, comemore!!')
+
+elif 2000 < Salario <= 3500:
+    imposto = Salario * 0.10
+    print(f'Seu imposto é de {imposto}!')
+
+elif Salario > 3500:
+    imposto = Salario * 0.15
+    print(f'O imposto é de {imposto}!')
